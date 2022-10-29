fd31eba352bec2991cfdb1966b4f77580f2301cd
Ricardo Souza <ricardo.souza@hootsuite.com>
2022-10-28 22:09:13 -0700

    Convert TestePratico.Common to .net core 6

--- TestePratico.Commom/Ninject/ModuleHelper.cs
+++ TestePratico.Commom/Ninject/ModuleHelper.cs
@@ -1,27 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Ninject.Modules;
+﻿using Ninject.Modules;
 using TestePratico.Commom.Ninject.Modules;
 
 namespace TestePratico.Commom.Ninject
 {
-	public class ModuleHelper
-	{
-		private ModuleHelper()
-		{
-			throw new InvalidOperationException("Não é permitido instanciar esta classe.");
-		}
+    public class ModuleHelper
+    {
+        private ModuleHelper()
+        {
+            throw new InvalidOperationException("Não é permitido instanciar esta classe.");
+        }
 
-		public static IList<INinjectModule> GetModules()
-		{
-			return new List<INinjectModule>()
-			{
-				new DataModule(),
-				new AppModule()
-			};
-		}
-	}
+        public static IList<INinjectModule> GetModules()
+        {
+            return new List<INinjectModule>()
+            {
+                new DataModule(),
+                new AppModule()
+            };
+        }
+    }
 }
--- TestePratico.Commom/Ninject/Modules/AppModule.cs
+++ TestePratico.Commom/Ninject/Modules/AppModule.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Ninject.Modules;
+﻿using Ninject.Modules;
 using TestePratico.Application;
 using TestePratico.Application.Interfaces;
-using TestePratico.Domain.Interfaces;
 using TestePratico.Domain.Interfaces.Services;
 using TestePratico.Domain.Services;
 
 namespace TestePratico.Commom.Ninject.Modules
 {
-	public class AppModule : NinjectModule
-	{
-		public override void Load()
-		{
-			Bind(typeof(IAppServiceBase<>)).To(typeof(AppServiceBase<>));
-			Bind<IPessoaAppService>().To<PessoaAppService>();
-			Bind<IUFAppService>().To<UFAppService>();
+    public class AppModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind(typeof(IAppServiceBase<>)).To(typeof(AppServiceBase<>));
+            Bind<IPessoaAppService>().To<PessoaAppService>();
+            Bind<IUFAppService>().To<UFAppService>();
 
-			Bind(typeof(IServiceBase<>)).To(typeof(ServiceBase<>));
-			Bind<IPessoaService>().To<PessoaService>();
-			Bind<IUFService>().To<UFService>();
-		}
-	}
+            Bind(typeof(IServiceBase<>)).To(typeof(ServiceBase<>));
+            Bind<IPessoaService>().To<PessoaService>();
+            Bind<IUFService>().To<UFService>();
+        }
+    }
 }
