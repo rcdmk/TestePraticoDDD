952d3d15689d434a26463d24c0efcf8b8a7c4a83
Ricardo Souza <ricardo.souza@hootsuite.com>
2022-11-02 22:02:26 -0700

    Clean up Domain layer

--- TestePratico.Domain/Entities/Pessoa.cs
+++ TestePratico.Domain/Entities/Pessoa.cs
@@ -6,11 +6,31 @@ namespace TestePratico.Domain.Entities
 {
     public class Pessoa : IValidable
     {
+        public Pessoa() : this(0, "", "", "", "", null!, new ValidationResult())
+        {
+        }
+
+        public Pessoa(int pessoaId, string nome, string ddd, string telefone, string email, UF uf, ValidationResult validationResult)
+        {
+            this.PessoaId = pessoaId;
+            this.Nome = nome;
+            this.DDD = ddd;
+            this.Telefone = telefone;
+            this.Email = email;
+            this.UF = uf;
+            this.ValidationResult = validationResult;
+        }
+
         public int PessoaId { get; set; }
+
         public string Nome { get; set; }
+
         public DateOnly? DataNascimento { get; set; }
+
         public string DDD { get; set; }
+
         public string Telefone { get; set; }
+
         public string Email { get; set; }
 
         public int? UFId { get; set; }
--- TestePratico.Domain/Entities/UF.cs
+++ TestePratico.Domain/Entities/UF.cs
@@ -7,11 +7,26 @@ namespace TestePratico.Domain.Entities
 {
     public class UF : IValidable
     {
+        public UF() : this(0, "", new List<Pessoa>(), new ValidationResult())
+        {
+        }
+
+        public UF(int ufId, string nome, IList<Pessoa> Pessoas, ValidationResult validationResult)
+        {
+            this.UFId = ufId;
+            this.Nome = nome;
+            this.NumPessoas = Pessoas.Count;
+            this.Pessoas = Pessoas;
+            this.ValidationResult = validationResult;
+        }
+
         public int UFId { get; set; }
+
         public string Nome { get; set; }
 
         [NotMapped]
         public virtual int NumPessoas { get; set; }
+
         public virtual IList<Pessoa> Pessoas { get; set; }
 
         public ValidationResult ValidationResult { get; private set; }
