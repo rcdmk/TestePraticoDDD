e3592b4550045a434ca2bcfbb52fae7233f4c231
Ricardo Souza <ricardo.souza@hootsuite.com>
2022-11-09 22:39:52 -0800

    Implement UF update on Services layer

--- TestePratico.Services/AutoMapper/ViewModelToDomainMappingProfile.cs
+++ TestePratico.Services/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using TestePratico.Domain.Entities;
 
 namespace TestePratico.Services.AutoMapper
 {
@@ -7,6 +6,10 @@ namespace TestePratico.Services.AutoMapper
     {
         public ViewModelToDomainMappingProfile() : base(nameof(ViewModelToDomainMappingProfile))
         {
+            CreateMap<Services.Pessoa, Domain.Entities.Pessoa>().ForMember(p => p.PessoaId, o => o.MapFrom(p => p.Id));
+
+            CreateMap<Services.UF, Domain.Entities.UF>().ForMember(u => u.UFId, o => o.MapFrom(u => u.Id));
+            CreateMap<Services.UpdateUFRequest, Domain.Entities.UF>().ForMember(u => u.UFId, o => o.MapFrom(u => u.Id));
         }
     }
 }
--- TestePratico.Services/Services/UFsService.cs
+++ TestePratico.Services/Services/UFsService.cs
@@ -64,4 +64,22 @@ public class UFsService : UFs.UFsBase
 
         return Task.FromResult(response);
     }
+
+    public override Task<UpdateUFResponse> Update(UpdateUFRequest request, ServerCallContext context)
+    {
+        var existing = ufAppService.GetById(request.Id);
+        if (existing == null) throw new RpcException(new Status(StatusCode.NotFound, "Not Found"));
+
+        var uf = Mapper.Map<Domain.Entities.UF>(request);
+
+        if (!uf.IsValid) throw new RpcException(new Status(StatusCode.InvalidArgument, uf.ValidationResult!.ToString()));
+
+        var result = ufAppService.Update(uf);
+
+        if (!result.IsValid) throw new RpcException(new Status(StatusCode.InvalidArgument, result.ToString()));
+
+        ufAppService.SaveChanges();
+
+        return Task.FromResult(new UpdateUFResponse());
+    }
 }
