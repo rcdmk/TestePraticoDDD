7c17dcddbcbfb10be26e40009de76b14017f67ac
Ricardo Souza <ricardo.souza@hootsuite.com>
2022-11-09 22:37:58 -0800

    Remove unnecessary mappings for Grpc collections

--- TestePratico.Services/AutoMapper/DomainToViewModelMappingProfile.cs
+++ TestePratico.Services/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -1,6 +1,4 @@
 ﻿using AutoMapper;
-using Google.Protobuf.Collections;
-using TestePratico.Domain.Entities;
 
 namespace TestePratico.Services.AutoMapper
 {
@@ -8,15 +6,10 @@ namespace TestePratico.Services.AutoMapper
     {
         public DomainToViewModelMappingProfile() : base(nameof(DomainToViewModelMappingProfile))
         {
-            CreateMap<Domain.Entities.Pessoa, Services.Pessoa>();
-            CreateMap<IEnumerable<Domain.Entities.Pessoa>, RepeatedField<Services.Pessoa>>()
-                .ConvertUsing<ListDomainToGrpcResponseConverter<Domain.Entities.Pessoa, RepeatedField<Services.Pessoa>, Services.Pessoa>>();
+            CreateMap<Domain.Entities.Pessoa, Services.Pessoa>().ForMember(p => p.Id, o => o.MapFrom(p => p.PessoaId));
 
             CreateMap<Domain.Entities.UF, Services.PessoaUF>().ForMember(u => u.Id, o => o.MapFrom(u => u.UFId));
-
-            CreateMap<Domain.Entities.UF, Services.UF>();
-            CreateMap<IEnumerable<Domain.Entities.UF>, RepeatedField<Services.UF>>()
-                .ConvertUsing<ListDomainToGrpcResponseConverter<Domain.Entities.UF, RepeatedField<Services.UF>, Services.UF>>();
+            CreateMap<Domain.Entities.UF, Services.UF>().ForMember(u => u.Id, o => o.MapFrom(u => u.UFId));
         }
     }
 }
