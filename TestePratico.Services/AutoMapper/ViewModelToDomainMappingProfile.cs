424ea54acc3a2a31390180ccaa1ba142fefc21a3
Ricardo Souza <ricardo.souza@hootsuite.com>
2022-11-11 23:32:15 -0800

    Implement CRUD operations for Pessoa in Service

--- TestePratico.Services/AutoMapper/ViewModelToDomainMappingProfile.cs
+++ TestePratico.Services/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -8,12 +8,18 @@ namespace TestePratico.Services.AutoMapper
         public ViewModelToDomainMappingProfile() : base(nameof(ViewModelToDomainMappingProfile))
         {
             CreateMap<Services.Pessoa, Domain.Entities.Pessoa>();
+            CreateMap<Services.CreatePessoaRequest, Domain.Entities.Pessoa>();
+            CreateMap<Services.UpdatePessoaRequest, Domain.Entities.Pessoa>();
+
 
             CreateMap<Services.UF, Domain.Entities.UF>();
             CreateMap<Services.UpdateUFRequest, Domain.Entities.UF>();
 
             this.Internal().ForAllMaps((typeMap, mappingExpression) =>
-                mappingExpression.ForMember(typeMap.DestinationType.Name + "Id", o => o.MapFrom("Id"))
+            {
+                if (typeMap.Types.SourceType != typeof(Services.CreatePessoaRequest))
+                    mappingExpression.ForMember(typeMap.DestinationType.Name + "Id", o => o.MapFrom("Id"));
+            }
             );
         }
     }
--- TestePratico.Services/Services/PessoasService.cs
+++ TestePratico.Services/Services/PessoasService.cs
@@ -30,4 +30,70 @@ public class PessoasService : Pessoas.PessoasBase
 
         return Task.FromResult(response);
     }
+
+
+    public override Task<GetPessoaByIdResponse> GetById(GetPessoaByIdRequest request, ServerCallContext context)
+    {
+        var pessoa = pessoaAppService.GetById(request.Id);
+        if (pessoa == null) throw new RpcException(new Status(StatusCode.NotFound, "Not Found"));
+
+        var response = new GetPessoaByIdResponse()
+        {
+            Pessoa = Mapper.Map<Pessoa>(pessoa)
+        };
+
+        return Task.FromResult(response);
+    }
+
+    public override Task<CreatePessoaResponse> Create(CreatePessoaRequest request, ServerCallContext context)
+    {
+        var pessoa = Mapper.Map<Domain.Entities.Pessoa>(request);
+
+        if (!pessoa.IsValid) throw new RpcException(new Status(StatusCode.InvalidArgument, pessoa.ValidationResult!.ToString()));
+
+        var result = pessoaAppService.Add(pessoa);
+
+        if (!result.IsValid) throw new RpcException(new Status(StatusCode.InvalidArgument, result.ToString()));
+
+        pessoaAppService.SaveChanges();
+
+        var response = new CreatePessoaResponse()
+        {
+            Id = pessoa.PessoaId
+        };
+
+        return Task.FromResult(response);
+    }
+
+    public override Task<UpdatePessoaResponse> Update(UpdatePessoaRequest request, ServerCallContext context)
+    {
+        var existing = pessoaAppService.GetById(request.Id);
+        if (existing == null) throw new RpcException(new Status(StatusCode.NotFound, "Not Found"));
+
+        var pessoa = Mapper.Map(request, existing);
+
+        if (!pessoa.IsValid) throw new RpcException(new Status(StatusCode.InvalidArgument, pessoa.ValidationResult!.ToString()));
+
+        var result = pessoaAppService.Update(pessoa);
+
+        if (!result.IsValid) throw new RpcException(new Status(StatusCode.InvalidArgument, result.ToString()));
+
+        pessoaAppService.SaveChanges();
+
+        return Task.FromResult(new UpdatePessoaResponse());
+    }
+
+    public override Task<DeletePessoaResponse> Delete(DeletePessoaRequest request, ServerCallContext context)
+    {
+        var existing = pessoaAppService.GetById(request.Id);
+        if (existing == null) throw new RpcException(new Status(StatusCode.NotFound, "Not Found"));
+
+        var result = pessoaAppService.Remove(existing);
+
+        if (!result.IsValid) throw new RpcException(new Status(StatusCode.InvalidArgument, result.ToString()));
+
+        pessoaAppService.SaveChanges();
+
+        return Task.FromResult(new DeletePessoaResponse());
+    }
 }
