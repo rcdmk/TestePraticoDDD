7b75d3580e218975c7a773f94dfffcddab461004
Ricardo Souza <ricardo.souza@hootsuite.com>
2022-11-11 23:31:08 -0800

    Add validation for name length to Pessoa entity

--- TestePratico.Domain/Entities/Validation/PessoaValidator.cs
+++ TestePratico.Domain/Entities/Validation/PessoaValidator.cs
@@ -8,6 +8,7 @@ namespace TestePratico.Domain.Entities.Validation
         public PessoaValidator()
         {
             AddRule(new ValidationRule<Pessoa>(new PessoaPrecisaTerUmNomeDefinidoSpec(), "É obrigatório informar um nome"));
+            AddRule(new ValidationRule<Pessoa>(new PessoaPrecisaTerUmNomeEntre2e150Caracteres(), "O nome precisa ter entre 2 e 150 caracteres"));
             AddRule(new ValidationRule<Pessoa>(new PessoaPrecisaTerUmaUFDefinidaSpec(), "É obrigatório selecionar uma UF"));
             AddRule(new ValidationRule<Pessoa>(new PessoaNaoDeveTerDataDeNascimentoNoFuturoSpec(), "A data de nascimento não pode ser no futuro"));
         }
