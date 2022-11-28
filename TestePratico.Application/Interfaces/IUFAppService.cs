e9f365c880bad7a833b403a19f6f701986ac48e7
Ricardo Souza <ricardo.souza@hootsuite.com>
2022-11-28 09:08:43 -0800

    Translate Application layer

--- TestePratico.Application/Interfaces/IUFAppService.cs
+++ TestePratico.Application/Interfaces/IUFAppService.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TestePratico.Domain.Entities;
+﻿using TestePratico.Domain.Entities;
 
 namespace TestePratico.Application.Interfaces
 {
-	public interface IUFAppService : IAppServiceBase<UF>
-	{
-	}
+    public interface IUFAppService : IAppServiceBase<UF>
+    {
+    }
 }
